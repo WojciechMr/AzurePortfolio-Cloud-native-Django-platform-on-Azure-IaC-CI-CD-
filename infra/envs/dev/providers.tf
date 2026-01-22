3ae6b1701299108f27aa5be8929f5c4ffd209a4d
Wojciech Mrożek <wojtekmrozek717@gmail.com>
2026-01-22 19:02:16 +0100

    change infra/envs/dev/providers.tf

--- infra/envs/dev/providers.tf
+++ infra/envs/dev/providers.tf
@@ -1,12 +1,15 @@
 terraform {
   required_version = ">= 1.5.0"
 
-  backend "azurerm" {
-    resource_group_name  = "rg-AzurePortfolio-tfstate"
-    storage_account_name = "stazureportfoliodevtf"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-  }
+backend "azurerm" {
+  resource_group_name  = "rg-AzurePortfolio-tfstate"
+  storage_account_name = "stazureportfoliodevtf"
+  container_name       = "tfstate"
+  key                  = "dev.terraform.tfstate"
+
+  use_azuread_auth = true
+}
+
 
   required_providers {
     azurerm = {
